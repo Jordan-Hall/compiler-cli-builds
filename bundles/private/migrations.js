d0e7b576396e1899114e459b491c2724a08d68fd
Jordan Hall <jordan@libertyware.co.uk>
2024-10-29 13:16:52 +0000

    af2eb6dfb1 feat(compiler-cli): add support for automatic imports for standalone

---
+++ bundles/private/migrations.js
@@ -0,0 +1,41 @@
+
+      import {createRequire as __cjsCompatRequire} from 'module';
+      const require = __cjsCompatRequire(import.meta.url);
+    
+import {
+  DynamicValue,
+  PartialEvaluator,
+  PotentialImportKind,
+  PotentialImportMode,
+  StaticInterpreter,
+  forwardRefResolver
+} from "../chunk-2PLG6D2Y.js";
+import {
+  ImportManager,
+  Reference,
+  TypeScriptReflectionHost,
+  reflectObjectLiteral
+} from "../chunk-XXUXWFXO.js";
+import "../chunk-XSNUHRLJ.js";
+import "../chunk-UJ2J6WV4.js";
+import "../chunk-XI2RTGAL.js";
+export {
+  DynamicValue,
+  ImportManager,
+  PartialEvaluator,
+  PotentialImportKind,
+  PotentialImportMode,
+  Reference,
+  StaticInterpreter,
+  TypeScriptReflectionHost,
+  forwardRefResolver,
+  reflectObjectLiteral
+};
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.dev/license
+ */
+//# sourceMappingURL=migrations.js.map
---
+++ bundles/private/tooling.js
@@ -0,0 +1,21 @@
+
+      import {createRequire as __cjsCompatRequire} from 'module';
+      const require = __cjsCompatRequire(import.meta.url);
+    
+import {
+  GLOBAL_DEFS_FOR_TERSER,
+  GLOBAL_DEFS_FOR_TERSER_WITH_AOT,
+  constructorParametersDownlevelTransform
+} from "../chunk-PLUKKCTP.js";
+import "../chunk-4JBNZ7YM.js";
+import "../chunk-2PLG6D2Y.js";
+import "../chunk-XXUXWFXO.js";
+import "../chunk-XSNUHRLJ.js";
+import "../chunk-UJ2J6WV4.js";
+import "../chunk-XI2RTGAL.js";
+export {
+  GLOBAL_DEFS_FOR_TERSER,
+  GLOBAL_DEFS_FOR_TERSER_WITH_AOT,
+  constructorParametersDownlevelTransform
+};
+//# sourceMappingURL=tooling.js.map
